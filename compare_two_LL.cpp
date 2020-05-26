96990485cf6f861a6aefd41ce43a8c5495e5a71a
Roshan Varghese <roshan.varghese612@gmail.com>
2020-05-26 08:52:43 +0530

    Create compare_two_LL.cpp

---
+++ compare_two_LL.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the compare_lists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    int flag = 0;
+    SinglyLinkedListNode *curr1 = head1;
+    SinglyLinkedListNode *curr2 = head2;
+    while(curr1 != NULL && curr2 != NULL){
+        cout<<"CURR1: "<<curr1->data<<endl;
+        cout<<"CURR2: "<<curr2->data<<endl;
+        if(curr1->data != curr2->data){
+            return 0;
+        }
+        curr1 = curr1->next;
+        curr2 = curr2->next;
+    }
+    cout<<"END"<<endl;
+    return 1; 
+
+}
+
+int main()
